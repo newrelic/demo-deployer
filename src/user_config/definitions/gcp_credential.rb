f0874a64993c265f96e78bde694bc3a2e106cf01
Julien4218 <jerard@newrelic.com>
2020-09-19 11:41:39 -0700

    Fix gcp credential

--- src/user_config/definitions/gcp_credential.rb
+++ src/user_config/definitions/gcp_credential.rb
@@ -1,46 +1,52 @@
 require "fileutils"
+require_relative 'credential'
 
 module UserConfig
   module Definitions
-    class GcpCredential
+    class GcpCredential < Credential
 
-      def initialize (user_config_query_lambda)
+      def initialize (provider, user_config_query_lambda)
+        @provider = provider
         @user_config_query_lambda = user_config_query_lambda
       end
 
       def get_auth_kind()
-        return @user_config_query_lambda.call("auth_kind")
+        return query("auth_kind")
       end
 
       def get_service_account_email()
-        return @user_config_query_lambda.call("service_account_email")
+        return query("service_account_email")
       end
 
       def get_service_account_file()
-        return @user_config_query_lambda.call("service_account_file")
+        return query("service_account_file")
       end
 
       def get_project()
-        return @user_config_query_lambda.call("project")
+        return query("project")
       end
 
       def get_scopes()
-        return @user_config_query_lambda.call("scopes")
+        return query("scopes")
       end
 
       def get_region()
-        return @user_config_query_lambda.call("region")
+        return query("region")
+      end
+
+      def get_secret_key_path()
+        return nil
       end
       
       def to_h()
-        return {
-          "auth_kind": get_auth_kind(),
-          "service_account_email": get_service_account_email(),
-          "service_account_file": get_service_account_file(),
-          "project": get_project(),
-          "scopes": get_scopes(),
-          "region": get_region()
-        }
+        items = {}
+        add_if_exist(items, "auth_kind", get_auth_kind())
+        add_if_exist(items, "service_account_email", get_service_account_email())
+        add_if_exist(items, "service_account_file", get_service_account_file())
+        add_if_exist(items, "project", get_project())
+        add_if_exist(items, "scopes", get_scopes())
+        add_if_exist(items, "region", get_region())
+        return items
       end
 
     end
