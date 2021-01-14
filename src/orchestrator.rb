f271086fc57c63f7374e6b9928af306d2581b306
Kris Kenney <70179215+nr-kkenney@users.noreply.github.com>
2021-01-14 15:03:44 -0800

    [DEMO-2521] Hook up footnote code to deployer
    * Hook up the previously created orchestrator and validator classes so
    that footnote validation and execution occur as part of a deployment.

--- src/orchestrator.rb
+++ src/orchestrator.rb
@@ -3,6 +3,7 @@ require './src/provision/orchestrator'
 require './src/install/orchestrator'
 require './src/teardown/orchestrator'
 require './src/summary/orchestrator'
+require './src/footnote/orchestrator'
 
 class Orchestrator
 
@@ -10,13 +11,14 @@ class Orchestrator
       context,
       configuration_orchestrator = nil, provision_orchestrator = nil,
       install_orchestrator = nil, teardown_orchestrator = nil,
-      summary_orchestrator = nil)
+      summary_orchestrator = nil, footnote_orchestrator = nil)
     @context = context
     @configuration_orchestrator = configuration_orchestrator || ConfigurationOrchestrator.new(context)
     @provision_orchestrator = provision_orchestrator || Provision::Orchestrator.new(context)
     @install_orchestrator = install_orchestrator || Install::Orchestrator.new(context)
     @teardown_orchestrator = teardown_orchestrator || Teardown::Orchestrator.new(context)
     @summary_orchestrator = summary_orchestrator || Summary::Orchestrator.new(context)
+    @footnote_orchestrator = footnote_orchestrator || Footnote::Orchestrator.new(context)
   end
 
   def execute(arguments = ARGV)
@@ -26,7 +28,8 @@ class Orchestrator
     else
       @provision_orchestrator.execute()
       @install_orchestrator.execute()
-      return @summary_orchestrator.execute()
+      @summary_orchestrator.execute()
+      return @footnote_orchestrator.execute()
     end
   end
 
