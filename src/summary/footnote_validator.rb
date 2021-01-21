b4f1912729fb3d1903ccb7fd0df0d70a70eb5afc
Kris Kenney <70179215+nr-kkenney@users.noreply.github.com>
2021-01-21 10:20:12 -0800

    [DEMO-2521] Remove code documentation comments

--- src/summary/footnote_validator.rb
+++ src/summary/footnote_validator.rb
@@ -1,15 +1,11 @@
 require './src/common/json_parser'
 
 module Summary
-  # Class responsible for validating the 'footnote' field of a deployment config.
   class FootnoteValidator
-    # @param [Common::JsonParser] json
     def initialize(json = nil)
       @json = json || Common::JsonParser.new
     end
 
-    # Run validation
-    # @param [Method] deploy_config_func - Method which can be called to return the deploy_config represented as a [Hash].
     def execute(deploy_config_func)
       deploy_config = deploy_config_func.call
       raw_footnote = deploy_config.fetch('output', {})['footnote']
@@ -18,9 +14,6 @@ module Summary
 
     private
 
-    # Validate that raw_footnote is either nil, a string, or an array of strings.
-    # @param [String, Array<string>] raw_footnote
-    # @return [Boolean] value indicating whether input is valid. true indicates valid, false indicates invalid.
     def validate(raw_footnote)
       if raw_footnote.is_a?(String)
         return true
