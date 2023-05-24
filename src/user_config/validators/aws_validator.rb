9e860ff66746eaef2a04542bf3d7f2faf1931f8b
Julien Erard <jerard@newrelic.com>
2023-05-24 10:30:14 -0700

    chore: add specific aws key validation with session token usage

--- src/user_config/validators/aws_validator.rb
+++ src/user_config/validators/aws_validator.rb
@@ -1,25 +1,29 @@
 require "./src/common/validators/file_exist"
 require "./src/common/validators/validator"
+require_relative "aws/api_key_validator"
 
 module UserConfig
   module Validators
     class AwsValidator
 
       def initialize(
-          api_key_validator = nil,
+          api_key_exist_validator = nil,
           secret_key_validator = nil,
-          region_validator = nil
+          region_validator = nil,
+          api_key_validator = nil
         )
-        @api_key_validator = api_key_validator || Common::Validators::FieldExistValidator.new("apiKey", "apiKey is missing for aws credential:")
+        @api_key_exist_validator = api_key_exist_validator || Common::Validators::FieldExistValidator.new("apiKey", "apiKey is missing for aws credential:")
         @secret_key_validator = secret_key_validator || Common::Validators::FieldExistValidator.new("secretKey", "secretKey is missing for aws credential:")
         @region_validator = region_validator || Common::Validators::FieldExistValidator.new("region", "region is missing for aws credential:")
+        @api_key_validator = api_key_validator || UserConfig::Validators::Aws::ApiKeyValidator.new()
       end
 
       def execute(aws_configs)
         validators = [
-          lambda { return @api_key_validator.execute(aws_configs) },
+          lambda { return @api_key_exist_validator.execute(aws_configs) },
           lambda { return @secret_key_validator.execute(aws_configs) },
-          lambda { return @region_validator.execute(aws_configs) }
+          lambda { return @region_validator.execute(aws_configs) },
+          lambda { return @api_key_validator.execute(aws_configs) }
         ]
 
         validator = Common::Validators::Validator.new(validators)
